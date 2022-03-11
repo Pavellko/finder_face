921c6ce909e4920564e157eb50b507b191a32b3b
Pavellko <pavelkollegov@gmail.com>
2022-03-11 10:40:35 +0300

    v1

--- finder.py
+++ finder.py
@@ -6,14 +6,12 @@ d=input("Введите каталог, где нужно определить �
 ff=0
 x = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
 spisok=[]
-vl=[]
+
 
 for dirpath, dirnames, filenames in os.walk(d):
     for filename in filenames:
     	if "jpg" in filename or "jpeg" in filename:
             file_path = os.path.join(dirpath, filename)
-            img = cv2.imread(file_path)
-            # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             det = x.detectMultiScale(img, 1.1, 19)
             if len(det)==0:
                 det_i=False
@@ -29,7 +27,6 @@ os.mkdir(copy_path_no)
 
 for ii in spisok:
     if ii[5]:
-        # cv2.imshow('rez', ii[1])
         shutil.copyfile(ii[0] , copy_path+'/'+ii[3] )
     else:
         shutil.copyfile(ii[0] , copy_path_no+'/'+ii[3] )
---
+++ finder2.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+import shutil
+
+x = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+
+img = cv2.imread('faces/001(1).jpg')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+det = x.detectMultiScale(img, 1.1, 19)
+for (x,y,w,h) in det:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), thickness=3)
+
+cv2.imshow('Результат', img)
+cv2.waitKey(0)

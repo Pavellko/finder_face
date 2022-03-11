86945dcb7fffed7593c79c6a60c3440cd66d13ff
Pavellko <pavelkollegov@gmail.com>
2022-03-11 11:53:35 +0300

    v7

--- finder_camera.py
+++ finder_camera.py
@@ -1,32 +1,21 @@
-import numpy as np
 import cv2
 
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+face_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
 
 while True:
-    ret, img = cap.read()
-    img = cv2.flip(img, -1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,     
-        scaleFactor=1.2,
-        minNeighbors=5,     
-        minSize=(20, 20)
-    )
+    success, img = cap.read()
+    #img = cv2.imread("IMG_20191012_145410_3.jpg")
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    faces = face_cascade_db.detectMultiScale(img_gray, 1.1, 19)
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]  
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0),2)
 
-    cv2.imshow('video',img)
-
-    k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
+    cv2.imshow('rez', img)
+    #cv2.waitKey()
+    if cv2.waitKey(1) & 0xff == ord('q'):
         break
 
 cap.release()
